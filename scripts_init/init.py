abea8ce7f298c892dc0a94a458599d71238ad216
Mario <mario.varlese3@studenti.unina.it>
2019-05-20 20:51:18 +0200

    ulteriore pulizia del codice e creazione di funzioni utili (moduli)

--- scripts_init/init.py
+++ scripts_init/init.py
@@ -19,6 +19,7 @@ from sklearn.ensemble import RandomForestRegressor
 
 
 def linear_reg(X_train,X_test,y_train,y_test):
+	print("Start linear regressor")
 	#regressore lineare
 	lm = linear_model.LinearRegression()
 	model = lm.fit(X_train, y_train)
@@ -26,27 +27,26 @@ def linear_reg(X_train,X_test,y_train,y_test):
 	return r2_score(y_test,y_pred)
 
 
-def KNN(X_train,X_test,y_train,y_test):
+def KNN_tun(X_train,X_test,y_train,y_test):
 	#K-neighbors regressor
+	print("start K nearest neighbor tuning hyperparameters function")
 	neighbors = list(range(10 , 1001 , 10));
 	r2 = [];
 	y_pred_neighbors = 0
 	#Standardize features by removing the mean and scaling to unit variance
 	
 	for n in neighbors:
-		neigh = KNeighborsRegressor(n_neighbors=n , metric='euclidean');
+		neigh = KNeighborsRegressor(n_neighbors=n);
 		neigh.fit(X_train , y_train);
 		y_pred_neighbors = neigh.predict(X_test); 
 		r2.append(r2_score(y_test,y_pred_neighbors));
-	#print(len(neighbors));
-	#print(len(r2));
-	plt.plot(neighbors , r2 , color = 'blue' , linewidth=3);
-	plt.grid(True);
-	plt.show();
+
+	#plt.plot(neighbors , r2 , color = 'blue' , linewidth=3);
+	#plt.grid(True);
+	#plt.show();
+
 	#get optimum K form the plot
 	max_r2,K_opt=opt_x(neighbors,r2)
-	print("R2_opt: ",max_r2)
-	print("K_opt: ", K_opt)
 
 
 	r2 = []
@@ -56,16 +56,24 @@ def KNN(X_train,X_test,y_train,y_test):
 		neigh.fit(X_train, y_train)
 		y_pred_neighbors = neigh.predict(X_test)
 		r2.append(r2_score(y_test,y_pred_neighbors))
-	plt.plot(distances , r2 , color = 'blue' , linewidth=3);
-	plt.show();
+	#plt.plot(distances , r2 , color = 'blue' , linewidth=3);
+	#plt.show();
+
 	#get optimum K form the plot
 	max_r2,opt_distmetr=opt_x(distances,r2)
-	#print("R2_opt: ",max_r2)
-	#print("opt_dist_metr: ", opt_distmetr)
+
+
+	#print("R2_knn",max_r2,"opt_dist_metr: ", opt_distmetr,"K_opt: ",K_opt)
 	return max_r2,opt_distmetr,K_opt
 
 
 
+def KNearestNeighbor(X_train,X_test,y_train,y_test,opt_distmetr,K_opt):
+	print("Start K nearest neighbor")
+	nn = KNeighborsRegressor(n_neighbors = K_opt , p=opt_distmetr)
+	nn.fit(X_train, y_train)
+	y_pred_neighbors = nn.predict(X_test)
+	return r2_score(y_test,y_pred_neighbors)
 
 #scatter plot delle feature e salva le figure -> in: 1. vettore feature name ( per le etichette del plot) 2. dataframe 
 def feature_plot(feature_vect,dataframe_x,y):
@@ -114,55 +122,57 @@ def get_feature(csv_file,feature_vect,y_label):
 
 
 
+def dataset_split(dataframe,y):
+	X_train , X_test , Y_train , Y_test = train_test_split(dataframe, y , test_size = 0.2 , random_state=42 ,  shuffle = True)
 
-
-
-def start_regression(dataframe,y):
-	#split dataset 20/80
-	X_train, X_test, y_train, y_test = train_test_split(dataframe, y, test_size=0.2 ,random_state=2 , shuffle=True);
-
-
-	#linear regressor
-	r2_lin=linear_reg(X_train, X_test, y_train, y_test)
-	print("R2_linearregressor: ",r2_lin)
-
-
-	#plot feature
-	#feature_plot(feature_vect,subdataframe,y)
-#	for i in range(0,len(feature_vect)-1):
-#		plt.scatter(X_test[: , i], y_test , linewidth=3)
-#		plt.plot(X_test , y_pred, color='green' , linewidth=3)
-#	plt.legend(['min_rsrp' , 'max_rsrp','median_rsrp' , 'min_rssi', 'max_rssi' , 'median_rssi' ,'y_pred'])
-#	plt.show()
-#	plt.savefig('feature_scatter_andytest.png')
-
-
-	#K-nearest-neighbor 
-	#se uso lo scaler l'r2 migliora di poco
 	scaler = StandardScaler();
 	scaler.fit(X_train);
 	X_train_scaled = scaler.transform(X_train);
 	X_test_scaled = scaler.transform(X_test);
-	r2_knn,dist,K=KNN(X_train_scaled,X_test_scaled,y_train,y_test)
-	print("R2_KNN: ",r2_knn)
+	return X_train , X_test , Y_train , Y_test
+
+def start_regression_tun(X_train, X_test, y_train, y_test):
+
+	
+	#K-nearest-neighbor 
+	r2_knn,dist,K=KNN_tun(X_train,X_test,y_train,y_test)
+	print("R2_KNN: ",r2_knn,"dispance_opt: ",dist,"K_opt: ",K)
+
+	#decision tree
+	max_depth_array=np.linspace(1,200,20,dtype=int)
+	minSamples_split=np.linspace(2,300,30,dtype=int)
+	r2dt,depth,samples_min=Tuning_DecisionTree_MaxDepth(max_depth_array ,minSamples_split, 
+		dataframe , y,X_train , X_test , Y_train , Y_test)
+	print("R2_DT: ",r2dt,"depth_opt: ",depth,"samples_min: ",samples_min)
+
+	#random forest
+	
+	num_trees_vect=np.linspace(1,200,dtype=int)
+	r2rf,maxfeat,ntrees=random_forest_tun(dataframe,y,num_trees_vect,depth,samples_min,X_train , X_test , Y_train , Y_test)
+	print("r2_rf: ",r2rf,"max_features_opt_RF: ",maxfeat,"num estimators opt RF: ",ntrees)
+
+
+def start_regression(X_train, X_test, y_train, y_test):
+	max_features_opt=1
+	n_estimators_opt=191
+	max_depth_opt=21
+	min_samples_split_opt=279
+	opt_distmetr=3
+	K_opt=40
+
+
+	r2lin=linear_reg(X_train, X_test, y_train, y_test)
+	r2knn=KNearestNeighbor(X_train,X_test,y_train,y_test,opt_distmetr,K_opt)
+	r2dt=DecisionTree(max_depth_opt,min_samples_split_opt,X_train, X_test, Y_train , Y_test)
+	r2rf=RandForest(max_depth_opt ,min_samples_split_opt ,max_features_opt, n_estimators_opt, X_train, X_test, Y_train , Y_test)
+
+	print("R2_linear_regressor: ",r2lin)
+	print("R2_Knearest_neighbor: ",r2knn)
+	print("R2_decision_tree: ",r2dt)
+	print("R2_Random_forest: ",r2rf)
 
 
 
-
-#le istruzioni show() dei plot sono commentate
-#sono state definite le seguenti funzioni: 
-
-#	feature_extract: fa un estrazione delle feature, per ora semplicemente prende in ingresso 
-#			0. Il path del csv contenente le feature e la colonna y
-#			1. la label delle feature x da estrarre (colonne)
-#			2. la label della y
-
-#	start_regression: effettua vari tipi di regressione: KNN, linear;
-
-
-#Dato un modello già tunato, il numero di splits , le feature X e l'uscita Y, se Y è continua allora Continous=True , altrimenti Continous = false
-#la seguente funzione effettua una StratifiedKFoldValidation discretizzando prima la variabile Y per effettuare la stratificazione.
-#La varibile di ritorno sarà il parametro caratteristico per indicare il fitting del modello dopo il processo di validazione
 def stratifiedKFold_validation(model , nsplits , continous , X , Y):
 	if continous==True:
 		bins = np.linspace(0,1,10);
@@ -182,21 +192,22 @@ def stratifiedKFold_validation(model , nsplits , continous , X , Y):
 
 #Dato un DecisionTreeRegressor , un array di interi contenente i valori possibili del max_depth , le features e le uscite del dataset la funzione effettua una grid cross 
 #validation per definire la max_depth migliore tra quelle che sono nell'array
-def Tuning_DecisionTree_MaxDepth(max_depth_array ,minSamples_split, X , Y):
+def Tuning_DecisionTree_MaxDepth(max_depth_array ,minSamples_split, X , Y,X_train , X_test , Y_train , Y_test):
+	print("Start decision tree tuning parameters function")
 	params_dt = {'max_depth': max_depth_array,'min_samples_split':minSamples_split} 
 
 	dt = DecisionTreeRegressor(random_state = 42)
 	grid_dt = GridSearchCV(estimator=dt, param_grid=params_dt , scoring = 'r2', n_jobs = -1 , cv = 5)
-	X_train , X_test , Y_train , Y_test = train_test_split(X, Y , test_size = 0.2 , random_state=42 ,  shuffle = True)
 	grid_dt.fit(X_train , Y_train)  
 	mxdp=grid_dt.best_params_['max_depth']
 	msp=grid_dt.best_params_['min_samples_split']
 	y_pred_DT=DecisionTree(mxdp,msp,X_train, X_test, Y_train , Y_test)
-	print("R2_dt:",r2_score(Y_test , y_pred_DT),"max_depth_opt: ",mxdp,"min_samples_split_opt: ",msp)
+	#print("R2_dt:",r2_score(Y_test , y_pred_DT),"max_depth_opt: ",mxdp,"min_samples_split_opt: ",msp)
 	return r2_score(Y_test , y_pred_DT),mxdp,msp
 
 #questa va chiamata dopo che è stata chiamata la funzione precedente e sono stati tarati i parametri max_depth e min_samples_split
-def random_forest_tun(X,Y,num_trees_vect,tuned_max_depth,tuned_min_samples_split):
+def random_forest_tun(X,Y,num_trees_vect,tuned_max_depth,tuned_min_samples_split,X_train , X_test , Y_train , Y_test):
+	print("Start random forest tuning parameters function")
 	numfeat=np.linspace(1,X.shape[1],X.shape[1]-1,dtype=int)
 	param_grid={
 		'max_features': numfeat,
@@ -204,47 +215,68 @@ def random_forest_tun(X,Y,num_trees_vect,tuned_max_depth,tuned_min_samples_split
 	}
 	rf=RandomForestRegressor(random_state = 42)
 	gdsc=GridSearchCV(estimator=rf,param_grid=param_grid, scoring = 'r2', n_jobs = -1 , cv = 5)
-	X_train , X_test , Y_train , Y_test = train_test_split(X, Y , test_size = 0.2 , random_state=42 ,  shuffle = True)
 	gdsc.fit(X_train , Y_train)
 	maxfeat=gdsc.best_params_['max_features']
 	ntrees=gdsc.best_params_['n_estimators']
-	print("max_features_opt_RF: ",maxfeat,"num estimators opt RF: ",ntrees)
+	#print("max_features_opt_RF: ",maxfeat,"num estimators opt RF: ",ntrees)
 	y_pred_RF=RandForest(tuned_max_depth,tuned_min_samples_split,maxfeat,ntrees,X_train, X_test, Y_train , Y_test)
-	print("R2_rf: ",r2_score(Y_test , y_pred_RF))
-	return r2_score(Y_test , y_pred_RF)
+	#print("R2_rf: ",r2_score(Y_test , y_pred_RF))
+	return r2_score(Y_test , y_pred_RF),maxfeat,ntrees
 
 
-#def random_forest(X,Y,num_trees_vect,num_feature_vect):
-#	return 
 
 def DecisionTree(max_depth_opt,min_samples_split_opt,X_train, X_test, Y_train , Y_test):
+	print("Start decision tree")
 	dt = DecisionTreeRegressor(random_state = 42, max_depth=max_depth_opt,min_samples_split=min_samples_split_opt)
 	dt.fit(X_train , Y_train)
 	y_pred_DT=dt.predict(X_test)
-	return y_pred_DT
+	return r2_score(Y_test , y_pred_DT)
 
 def RandForest(max_depth_opt ,min_samples_split_opt ,max_features_opt, n_estimators_opt, X_train, X_test, Y_train , Y_test):
+	print("Start random forest")
 	dt = RandomForestRegressor(max_depth=max_depth_opt , min_samples_split=min_samples_split_opt,
 		max_features=max_features_opt , n_estimators=n_estimators_opt , random_state = 42)
 	dt.fit(X_train , Y_train)
 	y_pred_RF=dt.predict(X_test)
-	return y_pred_RF
+	return r2_score(Y_test , y_pred_RF)
 
 
 
+#le istruzioni show() dei plot sono commentate
+#sono state definite le seguenti funzioni: 
 
-user="/home/mario/API"
-path="/QoS_RAILWAY_PATHS_REGRESSION/QoS_railway_paths_nodeid_iccid_feature_extraction.csv"
+#	feature_extract: fa un estrazione delle feature, per ora semplicemente prende in ingresso 
+#			0. Il path del csv contenente le feature e la colonna y
+#			1. la label delle feature x da estrarre (colonne)
+#			2. la label della y
 
-feature=['min_rsrp','max_rsrp','median_rsrp','min_rssi','max_rssi','median_rssi']
-y_label='res_dl_kbps'
-dataframe,y=get_feature(user+path,feature,y_label)
-#start_regression(dataframe,y)
-max_depth_array=np.linspace(1,200,20,dtype=int)
-minSamples_split=np.linspace(2,300,30,dtype=int)
-r2dt,depth,samples_min=Tuning_DecisionTree_MaxDepth(max_depth_array ,minSamples_split, dataframe , y)
-num_trees_vect=np.linspace(1,200,dtype=int)
-#depth=21
-#samples_min=279
-r2rf=random_forest_tun(dataframe,y,num_trees_vect,depth,samples_min)
+#	start_regression: effettua vari tipi di regressione: KNN, linear;
+
+
+#Dato un modello già tunato, il numero di splits , le feature X e l'uscita Y, se Y è continua allora Continous=True , altrimenti Continous = false
+#la seguente funzione effettua una StratifiedKFoldValidation discretizzando prima la variabile Y per effettuare la stratificazione.
+#La varibile di ritorno sarà il parametro caratteristico per indicare il fitting del modello dopo il processo di validazione
+
+
+if __name__=='__main__':
+	user="/home/mario/API"
+	path="/QoS_RAILWAY_PATHS_REGRESSION/QoS_railway_paths_nodeid_iccid_feature_extraction.csv"
+
+	feature=['min_rsrp','max_rsrp','median_rsrp','min_rssi','max_rssi','median_rssi']
+	y_label='res_dl_kbps'
+
+
+	dataframe,y=get_feature(user+path,feature,y_label)
+	#plot feature
+	#feature_plot(feature,subdataframe,y)
+	
+	X_train , X_test , Y_train , Y_test = dataset_split(dataframe,y)
+
+
+	
+	#start_regression_tun(X_train , X_test , Y_train , Y_test)
+	start_regression(X_train , X_test , Y_train , Y_test)
+
+
+	
 
