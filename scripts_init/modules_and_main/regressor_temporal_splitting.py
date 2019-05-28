233b72ce5cebfd0a51e3c2e88e9433d80c9ceccb
Mario <mario.varlese3@studenti.unina.it>
2019-05-28 16:19:30 +0200

    bellissima modifica

--- scripts_init/modules_and_main/regressor_temporal_splitting.py
+++ scripts_init/modules_and_main/regressor_temporal_splitting.py
@@ -2,13 +2,14 @@ import numpy as np
 import pandas as pd
 from preprocessing import get_main_features
 import os
-from splitting import dataset_split
+from splitting import dataset_split,stratifiedKFold_validation
 from regressors import start_regression_tun
 from regressors import start_regression
 from preprocessing import pca_preproc
 from preprocessing import get_feature
 import json
 import string as str
+from pandas.plotting import scatter_matrix
 
 
 def get_dataset_splittedby_time(range, time):
@@ -48,7 +49,7 @@ def read_tuning_par(filename):
 
 if __name__=='__main__':
 	step=2 #0=dataset creations 1=tuning&regression 2=regression without tuning
-	type_tec=1 #1=PCA 2=Kbest
+	type_tec=2 #1=PCA 2=Kbest
 	#funzione che genera i csv per lo splitting temporale, va chiamata una sola volta, poi commentata
 	if step==0:
 		temporal_splitting()
@@ -60,6 +61,10 @@ if __name__=='__main__':
 		#usando tutte le feature tranne quelle in feature_to_temove e PCA su dataset night
 		feature_vect, dataframe,y=get_feature("../QoS_RAILWAY_PATHS_REGRESSION/night.csv", feature_to_temove , y_label)
 		pca_df=pca_preproc(dataframe)
+
+		scatter_matrix(pca_df)
+		plt.show()
+
 		X_train_mean , X_test_mean , Y_train , Y_test = dataset_split(pca_df,y,False)
 		knn_dict_night , dt_dict_night , rf_dict_night=start_regression_tun(X_train_mean , X_test_mean, Y_train, Y_test)
 
@@ -68,6 +73,10 @@ if __name__=='__main__':
 		#usando tutte le feature tranne quelle in feature_to_temove e PCA su dataset day
 		feature_vect, dataframe,y=get_feature("../QoS_RAILWAY_PATHS_REGRESSION/day.csv", feature_to_temove , y_label)
 		pca_df=pca_preproc(dataframe)
+
+		scatter_matrix(pca_df)
+		plt.show()
+
 		X_train_mean , X_test_mean , Y_train , Y_test = dataset_split(pca_df,y,False)
 		knn_dict_day , dt_day , rf_dict_day=start_regression_tun(X_train_mean , X_test_mean, Y_train, Y_test)
 
@@ -84,17 +93,75 @@ if __name__=='__main__':
 		y_label='res_dl_kbps'
 
 		#usando tutte le feature tranne quelle in feature_to_temove e PCA su dataset night
+		feature_vect, dataframe,y=get_feature("../QoS_RAILWAY_PATHS_REGRESSION/day.csv", feature_to_temove , y_label)
+		pca_df=pca_preproc(dataframe)
+		stratifiedKFold_validation(True , pca_df , y,knn_dict , dt_dict , rf_dict)
+
+		#lettura parametri di tuning
+		knn_dict , dt_dict , rf_dict=read_tuning_par("Night_tunpcapar")
+
+
+		#usando tutte le feature tranne quelle in feature_to_temove e PCA su dataset night
 		feature_vect, dataframe,y=get_feature("../QoS_RAILWAY_PATHS_REGRESSION/night.csv", feature_to_temove , y_label)
 		pca_df=pca_preproc(dataframe)
-		X_train_mean , X_test_mean , Y_train , Y_test = dataset_split(pca_df,y,False)
-		dictR2=start_regression(X_train_mean , X_test_mean, 
-			Y_train, Y_test,knn_dict , dt_dict , rf_dict)
+		stratifiedKFold_validation(True , pca_df , y,knn_dict , dt_dict , rf_dict)
 
 
 	elif (step==1) & (type_tec==2):
-		print("ciao")
+		i=3
+		#funzioni di regressione
+		feature_to_remove= ['res_dl_kbps', 'ts_start', 'ts_end']
+		y_label='res_dl_kbps'
+
+		#night
+		x_mean, x_mode, y, main_feature_mean, main_feature_mode = get_main_features(
+			"../QoS_RAILWAY_PATHS_REGRESSION/night.csv" , feature_to_remove , y_label, i)
+	
+		scatter_matrix(x_mean)
+		plt.show()
+
+		X_train_mean , X_test_mean , Y_train , Y_test = dataset_split(x_mean,y,False)
+	
+
+		knn_dict , dt_dict , rf_dict = start_regression_tun(X_train_mean , X_test_mean , Y_train , Y_test)
 
 
+		save_tuning_par("Night_tunKbest",knn_dict,dt_dict,rf_dict)
 
 
+		#Morning
+		x_mean, x_mode, y, main_feature_mean, main_feature_mode = get_main_features(
+			"../QoS_RAILWAY_PATHS_REGRESSION/day.csv" , feature_to_remove , y_label, i)
+	
+		scatter_matrix(x_mean)
+		plt.show()
 
+		X_train_mean , X_test_mean , Y_train , Y_test = dataset_split(x_mean,y,False)
+	
+
+		knn_dict , dt_dict , rf_dict = start_regression_tun(X_train_mean , X_test_mean , Y_train , Y_test)
+
+
+		save_tuning_par("Day_tunKbest",knn_dict,dt_dict,rf_dict)
+
+	elif (step==2) & (type_tec==2):
+
+		#lettura parametri di tuning Night
+		knn_dict , dt_dict , rf_dict=read_tuning_par("Night_tunKbest")
+		i=3
+
+		#funzioni di regressione
+		feature_to_remove= ['res_dl_kbps', 'ts_start', 'ts_end']
+		y_label='res_dl_kbps'
+
+		x_mean, x_mode, y, main_feature_mean, main_feature_mode = get_main_features(
+			"../QoS_RAILWAY_PATHS_REGRESSION/night.csv" , feature_to_remove , y_label, i)
+
+		stratifiedKFold_validation(True , x_mean , y,knn_dict , dt_dict , rf_dict)
+
+		knn_dict , dt_dict , rf_dict=read_tuning_par("Day_tunKbest")
+
+		x_mean, x_mode, y, main_feature_mean, main_feature_mode = get_main_features(
+			"../QoS_RAILWAY_PATHS_REGRESSION/day.csv" , feature_to_remove , y_label, i)
+
+		stratifiedKFold_validation(True , x_mean , y,knn_dict , dt_dict , rf_dict)
