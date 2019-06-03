f9ad14bdafdf0c2747d27691a664ce96be54261e
andreafresa <andreafresa8@gmail.com>
2019-06-03 17:22:54 +0200

    aggiunto MLP

--- scripts_init/modules_and_main/multi_layer_perceptron.py
+++ scripts_init/modules_and_main/multi_layer_perceptron.py
@@ -5,23 +5,28 @@ from keras.utils import to_categorical
 from sklearn.metrics import accuracy_score,f1_score
 from numpy import mean
 import imblearn.metrics as im
- 
-def multi_layer_perceptron(samples_train, categorical_labels_train, samples_test, categorical_labels_test):
+import keras.wrappers.scikit_learn as wp
+from sklearn.model_selection import GridSearchCV 
+import numpy as np
+#def multi_layer_perceptron(samples_train, categorical_labels_train, samples_test, categorical_labels_test):
+def multi_layer_perceptron( num_nodes=100 ,activation = 'relu' ,input_dim = 19):
     '''
     Implements a multi-layer perceptron used as a baseline for DL-networks evaluation
     '''
-
+    
     print('Starting execution of single_layer_perceptron approach')
     # Total number of features
-    input_dim = samples_train.shape[1]
+
+
+    input_dim = input_dim
 
     # Number of classes
     num_classes = 4
 
     #Number of neurons
-    shallow_nodes_L1 = 100
-    shallow_nodes_L2 = 100
-    shallow_nodes_L3 = 100
+    shallow_nodes_L1 = num_nodes
+    shallow_nodes_L2 = num_nodes
+    shallow_nodes_L3 = num_nodes
 
     # Define a Sequential model
     model = Sequential()
@@ -29,10 +34,10 @@ def multi_layer_perceptron(samples_train, categorical_labels_train, samples_test
     # Build a multi-layer perceptron
     #Fully connected layers are defined using the Dense class.
     #Input layer
-    model.add(Dense(shallow_nodes_L1, activation = 'relu', input_dim = input_dim))
+    model.add(Dense(shallow_nodes_L1, activation = activation, input_dim = input_dim))
     #Hidden layer
-    model.add(Dense(shallow_nodes_L2, activation = 'softmax'))
-    model.add(Dense(shallow_nodes_L3, activation = 'softmax'))
+    model.add(Dense(shallow_nodes_L2, activation = activation))
+    model.add(Dense(shallow_nodes_L3, activation = activation))
     #Output layer
     model.add(Dense(num_classes, activation = 'softmax'))
     # model.summary()
@@ -40,20 +45,54 @@ def multi_layer_perceptron(samples_train, categorical_labels_train, samples_test
     print('single_layer_perceptron: Sequential model built')
 
     # Compile the model with categorical crossentropy loss function
-    # model.compile(loss='categorical_crossentropy', optimizer=sgd_opt, metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     # Compile the model with categorical crossentropy loss function and Adam optimizer
-    model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+    #model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
 
     print('single_layer_perceptron: Sequential model compiled with crossentropy loss function')
-
     # Plot DL architecture in PNG format
     #plot_model(model, to_file="single_layer_perceptron.png")
+    return model
+#def multi_layer_perceptron(samples_train, categorical_labels_train, samples_test, categorical_labels_test):
 
-    # Define early stopping callback
-    earlystop = EarlyStopping(monitor='acc', min_delta=0.0001, patience=10, verbose=1, mode='auto')
+def train_test(samples_train, categorical_labels_train, samples_test, categorical_labels_test, num_nodes , activation): 
+        # Define early stopping callback
+    input_dim = samples_train.shape[1]
+
+    # Number of classes
+    num_classes = 4
+
+    #Number of neurons
+    shallow_nodes_L1 = num_nodes
+    shallow_nodes_L2 = num_nodes
+    shallow_nodes_L3 = num_nodes
+
+    # Define a Sequential model
+    model = Sequential()
+
+    # Build a multi-layer perceptron
+    #Fully connected layers are defined using the Dense class.
+    #Input layer
+    model.add(Dense(shallow_nodes_L1, activation = activation,  input_dim = input_dim))
+    #Hidden layer
+    model.add(Dense(shallow_nodes_L2, activation = activation))
+    model.add(Dense(shallow_nodes_L3, activation = activation))
+    #Output layer
+    model.add(Dense(num_classes, activation = 'softmax'))
+    # model.summary()
+
+    print('single_layer_perceptron: Sequential model built')
+
+    # Compile the model with categorical crossentropy loss function
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # Compile the model with categorical crossentropy loss function and Adam optimizer
+    #model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+
+    print('single_layer_perceptron: Sequential model compiled with crossentropy loss function')
+    earlystop = EarlyStopping(monitor='acc', min_delta=0.001, patience=10, verbose=1, mode='auto')
     callbacks_list = [earlystop]
-
     # Training phase: train_model(model, epochs, batch_size, callbacks, samples_train, categorical_labels_train, num_classes)
     train_model(model, 200, 50, callbacks_list, samples_train, categorical_labels_train, num_classes)
     print('single_layer_perceptron: Training phase completed')
@@ -88,7 +127,7 @@ def test_model(model, samples_train, categorical_labels_train, samples_test, cat
 
     predictions = model.predict_classes(samples_test, verbose=2)
 
-    # Calculate soft predictions
+    # Calculate soft predictionsmulti_layer_perceptron
     soft_values = model.predict(samples_test, verbose=2)
 
     training_predictions = model.predict_classes(samples_train, verbose=2)
@@ -114,3 +153,19 @@ def test_model(model, samples_train, categorical_labels_train, samples_test, cat
     training_macro_gmean = mean(im.geometric_mean_score(categorical_labels_train, training_predictions, average=None))
 
     return soft_values, predictions, training_soft_values, training_predictions, accuracy, fmeasure, macro_gmean, training_accuracy, training_fmeasure, training_macro_gmean
+
+
+def mlp_tuning(X_train , X_test , Y_train , Y_test):
+    model = wp.KerasClassifier(build_fn = multi_layer_perceptron, input_dim = X_train.shape[1])
+
+    grid_params = {'activation': ['relu' , 'sigmoid', 'tanh'] , 'num_nodes' : np.arange(10 , 120 , 10)}
+    cv = GridSearchCV(estimator = model , param_grid = grid_params , n_jobs=-1 , verbose = 2 , cv = 5)
+    grid_result = cv.fit(X_train , Y_train)
+    print("Best: %f using %s" , (grid_result.best_params_))
+    numnodes=grid_result.best_params_['num_nodes']
+    activation=grid_result.best_params_['activation']
+    soft_values, predictions, training_soft_values, training_predictions, accuracy, fmeasure, macro_gmean, training_accuracy, training_fmeasure, training_macro_gmean=train_test(X_train , Y_train , X_test , Y_test , num_nodes=numnodes, activation=activation)
+    print(accuracy)
+
+    
+#'hidden_layer_sizes':np.arange(50, 150)
